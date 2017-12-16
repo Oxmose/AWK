caad83a78ddb374330b98e0886a90f457fa84cc1
Oxmose <alexy.torresa@gmail.com>
2017-12-16 11:50:45 -0500

    Added IRQs management functions

--- drivers/pic.c
+++ drivers/pic.c
@@ -13,9 +13,9 @@
  *
  ******************************************************************************/
 
-#include "../cpu/cpu.h" /* outb */
-#include "../lib/stdint.h" /* Generic int types */
-#include "../lib/stddef.h" /* OS_RETURN_E, NULL */
+#include "../cpu/cpu.h"            /* outb */
+#include "../lib/stdint.h"         /* Generic int types */
+#include "../lib/stddef.h"         /* OS_RETURN_E, NULL */
 #include "../core/kernel_output.h" /* kernel_success */
 
 /* Header file */
@@ -44,4 +44,74 @@ void setup_pic()
 	outb(0xFF, PIC_SLAVE_DATA_PORT);
 
 	kernel_success("PIC Initialized\n", 16);
+}
+
+OS_RETURN_E set_IRQ_mask(const uint32_t IRQ_number, const uint8_t enabled)
+{
+    if(IRQ_number > MAX_IRQ_LINE)
+    {
+        return OS_ERR_NO_SUCH_IRQ_LINE;
+    }
+
+    /* Manage master PIC */
+    if(IRQ_number < 8)
+    {
+        /* Retrieve initial mask */
+        uint8_t init_mask = inb(PIC_MASTER_DATA_PORT); 
+
+        /* Set new mask value */
+        if(!enabled)
+        {
+            init_mask |= 1 << IRQ_number;
+        }
+        else
+        {
+            init_mask &= ~(1 << IRQ_number);
+        }
+
+        /* Set new mask */
+        outb(init_mask, PIC_MASTER_DATA_PORT);
+    }
+
+    /* Manage slave PIC. WARNING, cascading has to be enabled */
+    if(IRQ_number > 7)
+    {
+        /* Set new IRQ number */
+         uint32_t cascading_number = IRQ_number - 8;
+
+        /* Retrieve initial mask */
+        uint8_t init_mask = inb(PIC_SLAVE_DATA_PORT); 
+
+        /* Set new mask value */
+        if(!enabled)
+        {
+            init_mask |= 1 << cascading_number;
+        }
+        else
+        {
+            init_mask &= ~(1 << cascading_number);
+        }
+
+        /* Set new mask */
+        outb(init_mask, PIC_SLAVE_DATA_PORT);
+    }
+
+    return OS_NO_ERR;
+}
+
+OS_RETURN_E set_IRQ_EOI(const uint32_t IRQ_number)
+{ 
+    if(IRQ_number > MAX_IRQ_LINE)
+    {
+        return OS_ERR_NO_SUCH_IRQ_LINE;
+    }
+    
+    /* End of interrupt signal */
+    if(IRQ_number > 7)
+    {
+        outb(PIC_EOI, PIC_SLAVE_COMM_PORT);
+    }
+    outb(PIC_EOI, PIC_MASTER_COMM_PORT);
+
+    return OS_NO_ERR;
 }
