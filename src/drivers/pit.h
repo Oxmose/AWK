abfc82a73cb86805395e7543493e7b1c27d48f7a
Oxmose <alexy.torresa@gmail.com>
2017-12-17 10:52:17 -0500

    PIT is now only used for scheduling. The PIT tick handler is the scheduler function

--- src/drivers/pit.h
+++ src/drivers/pit.h
@@ -10,6 +10,7 @@
  *
  * PIT (Programmable interval timer) driver
  * System main timer management. Allows to define system ticks.
+ * The PIT is only used to call the scheduler.
  *
  ******************************************************************************/
 
@@ -29,36 +30,14 @@
 #define PIT_COMM_PORT      0x34
 #define PIT_DATA_PORT      0x40
 #define PIT_COMM_SET_FREQ  0x43
-#define PIT_FREQ           100
+#define PIT_FREQ           19
 #define PIT_IRQ            0
 #define PIT_INTERRUPT_LINE 32
 
-/* System pit events settings */
-#define PIT_MAX_EVENT_COUNT 16
-
-/*****************************************
- * STRUCTURES
- ****************************************/
-
-/* Main pit event structure */
-struct pit_event
-{
-    void        (*execute)(void);
-    uint32_t    period;
-    OS_EVENT_ID event_id;
-
-    uint8_t enabled;
-};
-typedef struct pit_event pit_event_t;
 
 /************************************
  * FUNCTIONS 
  ***********************************/
-
-/* Clock tick handler, should only be called in an interrupt context */
-void pit_interrupt_handler(cpu_state_t *cpu_state, uint32_t int_id, 
-                           stack_state_t *stack_state);
-
 /* Init pit settings
  *
  * @returns The error or success state.
@@ -82,25 +61,5 @@ uint32_t get_tick_count(void);
  * @returns Tick count since the last call to this function
  */
 uint32_t get_cpu_tick_count(void);
-
-/* Register a new event to execute on pit tick on a defined period given as
- * parameter.
- *
- * @param function The routine to execute when the period is reached.
- * @param period The period at wich the event should be executed.
- * @param event_id the OS_EVENT_ID buffer to receive the event id, may be -1 on
- * error.
- * @returns The error code. 
- */
-OS_RETURN_E register_pit_event(void (*function)(void), 
-                                 const uint32_t period,
-                                 OS_EVENT_ID *event_id);
-
-/* Unregister a pit event based on the event id given as parameter.
- *
- * @param event_id The even id to be unregistered.
- * @returns The error code. 
- */
-OS_RETURN_E unregister_pit_event(const OS_EVENT_ID event_id);
 
 #endif /* __PIT_H_ */
