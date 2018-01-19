3c2f29a4e32ca26cf2eb57cd10ec2da8a463deb5
Oxmose <alexy.torresa@gmail.com>
2018-01-19 18:41:26 -0500

    Added recusive ability to mutex

--- src/tests/other/test_mutex.c
+++ src/tests/other/test_mutex.c
@@ -15,6 +15,36 @@ mutex_t mutex2;
 
 uint32_t lock_res;
 
+void* test_rec(void* args)
+{
+    (void)args;
+    if(mutex_pend(&mutex1))
+    {
+        printf("Failed to pend mutex1 1\n");
+        return (void*)0;
+    }
+
+    if((int)args == 0)
+    {
+        if(mutex_pend(&mutex1) != OS_ERR_MUTEX_UNINITIALIZED)
+        {
+            printf("Failed to pend mutex1 1\n");
+            return (void*)1;
+        }
+    }
+    else
+    {
+        if(mutex_pend(&mutex1) != OS_NO_ERR)
+        {
+            printf("Failed to pend mutex1 1\n");
+            return (void*)1;
+        }
+    }
+    printf(" (T R END) ");
+    return (void*)0;
+}
+
+
 void *mutex_thread_1(void *args)
 {
     for(int i = 0; i < 1000000; ++i)
@@ -102,12 +132,12 @@ void *mutex_thread_3(void *args)
 
 int test_mutex(void)
 {
-    if(mutex_init(&mutex1) != OS_NO_ERR)
+    if(mutex_init(&mutex1, MUTEX_FLAG_NONE) != OS_NO_ERR)
     {
         printf("Failed to init mutex1\n");
         return -1;
     }
-    if(mutex_init(&mutex2) != OS_NO_ERR)
+    if(mutex_init(&mutex2, MUTEX_FLAG_NONE) != OS_NO_ERR)
     {
         printf("Failed to init mutex2\n");
         return -1;
@@ -149,23 +179,76 @@ int test_mutex(void)
         return -1;
     }
 
-    if(mutex_destroy(&mutex1) != OS_NO_ERR)
+    /* Test non recursive mutex */
+    int ret;
+    if((create_thread(&thread_mutex1, test_rec, 1, "thread1", (void*)0)) != OS_NO_ERR)
     {
-        kernel_error("Failed to destroy mutex1\n");
+        kernel_error(" Error while creating the main thread!\n");
         return -1;
     }
+
+    sleep(1000);
+
+    if((err = mutex_destroy(&mutex1)) != OS_NO_ERR)
+    {
+        kernel_error("Failed to destroy mutex1 %d\n", err);
+        return -1;
+    }
+
+    if((err = wait_thread(thread_mutex1, (void*)&ret)) != OS_NO_ERR)
+    {
+        kernel_error("Error while waiting thread! [%d]\n", err);
+        return -1;
+    }
+    if(ret != 0)
+    {
+        return -1;
+    }
+
+    /* Test recursive mutex */
+    if(mutex_init(&mutex1, MUTEX_FLAG_RECURSIVE) != OS_NO_ERR)
+    {
+        printf("Failed to init mutex1\n");
+        return -1;
+    }
+
+    if((create_thread(&thread_mutex1, test_rec, 1, "thread1", (void*)1)) != OS_NO_ERR)
+    {
+        kernel_error(" Error while creating the main thread!\n");
+        return -1;
+    }
+
+
+    if((err = wait_thread(thread_mutex1, (void*)&ret)) != OS_NO_ERR)
+    {
+        kernel_error("Error while waiting thread! [%d]\n", err);
+        return -1;
+    }
+    if(ret != 0)
+    {
+        return -1;
+    }
+
+    if((err = mutex_destroy(&mutex1)) != OS_NO_ERR)
+    {
+        kernel_error("Failed to destroy mutex1 %d\n", err);
+        return -1;
+    }
+
     if(mutex_destroy(&mutex2) != OS_NO_ERR)
     {
         kernel_error("Failed to destroy mutex2\n");
         return -1;
     }
 
-
     if(wait_thread(thread_mutex3, NULL) != OS_NO_ERR)
     {
         printf("Error while waiting thread! [%d]\n", err);
         return -1;
     }
+
+    /* Test recusive mutex */
+
     printf("Lock res = %d\n", lock_res);
     return lock_res != 2000000;
 }
